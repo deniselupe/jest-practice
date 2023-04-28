a219600308a4fd6ca8774d3ce4c6675331f8f628
dgrodriguez24 <dgrodriguez24@hotmail.com>
2023-04-28 11:45:08 -0400

    expect .toHaveReturned matchers

--- expect-api/expectAPINotes.test.js
+++ expect-api/expectAPINotes.test.js
@@ -5,12 +5,12 @@
     For example, this code will validate some properties of the can object:
 */
 
-const can = {
-    name: 'pamplemousse',
-    ounces: 12
-};
-
 describe('the can', () => {
+    const can = {
+        name: 'pamplemousse',
+        ounces: 12
+    };
+
     test('has 12 ounces', () => {
         expect(can.ounces).toBe(12);
     });
@@ -46,13 +46,14 @@ describe('the can', () => {
 
     You can do that with this test suite:
 */
-function drinkAll(callback, flavour) {
-    if (flavour !== 'octopus') {
-        callback(flavour);
-    }
-}
 
 describe('drinkAll', () => {
+    function drinkAll(callback, flavour) {
+        if (flavour !== 'octopus') {
+            callback(flavour);
+        }
+    }
+
     test('drinks something lemon-flavoured', () => {
         const drink = jest.fn();
         drinkAll(drink, 'lemon');
@@ -78,11 +79,12 @@ describe('drinkAll', () => {
 
     You can do that with this test suite:
 */
-function drinkEach(callback, flavourArr) {
-    flavourArr.forEach(flavour => callback(flavour));
-}
 
 describe('drinkEach', () => {
+    function drinkEach(callback, flavourArr) {
+        flavourArr.forEach(flavour => callback(flavour));
+    }
+
     test('drinkEach drinks each drink', () => {
         const drink = jest.fn();
         drinkEach(drink, ['lemon', 'octopus']);
@@ -103,50 +105,50 @@ describe('drinkEach', () => {
     To make sure this works, you could write:
 */
 
-const registeredDrinks = [];
+describe('applyToAll', () => {
+    const registeredDrinks = [];
 
-class LaCroix {
-    #_flavor;
-    #_registered;
+    class LaCroix {
+        #_flavor;
+        #_registered;
 
-    constructor(flavor) {
-        this.#_flavor = flavor;
-        this.#_registered = false;
-    }
+        constructor(flavor) {
+            this.#_flavor = flavor;
+            this.#_registered = false;
+        }
 
-    get flavor() {
-        return this.#_flavor;
-    }
+        get flavor() {
+            return this.#_flavor;
+        }
 
-    get registered() {
-        return this.#_registered;
-    }
+        get registered() {
+            return this.#_registered;
+        }
 
-    set registered(value) {
-        if (typeof(value) === 'boolean') {
-            this.#_registered = value;
-        } else {
-            throw new Error('Value is not a boolean!');
+        set registered(value) {
+            if (typeof(value) === 'boolean') {
+                this.#_registered = value;
+            } else {
+                throw new Error('Value is not a boolean!');
+            }
         }
     }
-}
 
-function register(drink) {
-    drink.registered = true;
-    registeredDrinks.push(drink);
-}
-
-function applyToAll(f) {
-    if (!!registeredDrinks.length) {
-        return registeredDrinks.map((drink) => {
-            return f(drink);
-        });
+    function register(drink) {
+        drink.registered = true;
+        registeredDrinks.push(drink);
     }
 
-    return [];
-}
+    function applyToAll(f) {
+        if (!!registeredDrinks.length) {
+            return registeredDrinks.map((drink) => {
+                return f(drink);
+            });
+        }
 
-describe('applyToAll', () => {
+        return [];
+    }
+
     test('registration applies correctly to orange La Croix', () => {
         const beverage = new LaCroix('orange');
         register(beverage);
@@ -170,18 +172,181 @@ describe('applyToAll', () => {
     You can write:
 */
 
-function applyToAllFlavors(fn) {
-    const flavors = ['grape', 'strawberry', 'orange', 'mango'];
-
-    return flavors.map((flavor) => {
-        return fn(flavor);
-    });
-}
-
 describe('applyToAllFlavors', () => {
+    function applyToAllFlavors(fn) {
+        const flavors = ['grape', 'strawberry', 'orange', 'mango'];
+    
+        return flavors.map((flavor) => {
+            return fn(flavor);
+        });
+    }
+
     test('applying to all flavors does mango last', () => {
         const drink = jest.fn();
         applyToAllFlavors(drink);
         expect(drink).toHaveBeenLastCalledWith('mango');
+    });
+});
+
+/*
+    .toHaveBeenNthCalledWith(nthCall, arg1, arg2, ...)
+    Also under the alias: .nthCalledWith(nthCall, arg1, arg2, ...)
+
+    If you have a mock function, you can use `.toHaveBeenNthCalledWith` to 
+    test what arguments it was nth called with. 
+
+    Note:
+    The nth argument must be positive integer starting from 1.
+
+    For example, let's say you have a `drinkEachTwo(drink, Array<flavor>)` function 
+    that applies `f` to a bunch of flavors, and you want to ensure that when 
+    you call it, the first flavor it operates on is `lemon` and the second one is 
+    `octopus`. You can write:
+*/
+
+describe('drinkEachTwo', () => {
+    function drinkEachTwo(fn, flavors) {
+        flavors.forEach((flavor) => {
+            fn(flavor);
+        });
+    }
+
+    test('drinkEachTwo drinks lemon flavor first', () => {
+        const drink = jest.fn();
+        drinkEachTwo(drink, ['lemon', 'octopus']);
+        expect(drink).toHaveBeenNthCalledWith(1, 'lemon');
+    });
+
+    test('drinkEachTwo drinks octopus flavor second', () => {
+        const drink = jest.fn();
+        drinkEachTwo(drink, ['lemon', 'octopus']);
+        expect(drink).toHaveBeenNthCalledWith(2, 'octopus');
+    });
+});
+
+/*
+    .toHaveReturned()
+    Also under the alias: .toReturn()
+
+    If you have a mock function, you can use `.toHaveReturned` to test 
+    that the mock function successfully returned (i.e., did not throw an error) 
+    at least one time. 
+
+    For example, let's say you have a mock `drinks` that returns `true`. 
+    You can write:
+*/
+
+describe('drinks', () => {
+    test('drinks returns', () => {
+        const drinks = jest.fn(() => true);
+        drinks();
+        expect(drinks).toHaveReturned();
+    });
+});
+
+/*
+    .toHaveReturnedTimes(number)
+    Also under the alias: .toReturnTimes(number)
+
+    Use `.toHaveReturnedTimes` to ensure that a mock function returned 
+    successfully (i.e., did not throw an error) an exact number of times.
+
+    Any calls to the mock function that throw an error are not counted toward 
+    the number of times the function returned.
+
+    For example, let's say you have a mock `drinksTwo` function that returns `true`.
+    You can write:
+*/
+
+describe('drinksTwo', () => {
+    test('drinksTwo returns twice', () => {
+        const drinksTwo = jest.fn(() => true);
+        
+        drinksTwo();
+        drinksTwo();
+
+        expect(drinksTwo).toHaveReturnedTimes(2);
+    });
+});
+
+/*
+    .toHaveReturnedWith(value)
+    Also under the alias .toReturnWith(value)
+
+    Use `.toHaveReturnedWith` to ensure that a mock function 
+    returned a specific value.
+
+    For example, let's say you have a mock `drinksThree` that 
+    returns the name of the beverage that was consumed. You can 
+    write:
+*/
+
+describe('drinksThree', () => {
+    test('drinksThree returns La Croix', () => {
+        const beverage = { name: 'La Croix' };
+        const drinksThree = jest.fn((beverage) => beverage.name);
+        
+        drinksThree(beverage);
+        
+        expect(drinksThree).toHaveReturnedWith('La Croix');
+    });
+});
+
+/*
+    .toHaveLastReturnedWith(value)
+    Also under the alias: .lastReturnedWith(value)
+
+    Use `.toHaveLastReturnedWith` to test the specific value that a 
+    mock function last returned. If the last call to the mock function threw 
+    an error, then this matcher will fail no matter what value you provided as the 
+    expected return value.
+
+    For example, let's say you have a mock `drinksFour` that returns the 
+    name of the beverage that was consumed. 
+
+    You can write:
+*/
+
+describe('drinksFour', () => {
+    test('drinksFour returns La Croix (Orange) last', () => {
+        const beverage1 = { name: 'La Croix (Lemon)' };
+        const beverage2 = { name: 'La Croix (Orange)' };
+        const drinksFour = jest.fn((beverage) => beverage.name);
+
+        drinksFour(beverage1);
+        drinksFour(beverage2);
+
+        expect(drinksFour).toHaveLastReturnedWith('La Croix (Orange)');
+    });
+});
+
+/*
+    .toHaveNthReturnedWith(nthCall, value)
+    Also under the alias: .nthReturnedWith(nthCall, value)
+
+    Use `.toHaveNthReturnedWith` to test the specific value that a mock 
+    function returned for the nth call. If the nth call to the mock 
+    function threw an error, then this matcher will fail no matter what 
+    value you provided as the expected return value.
+
+    Note:
+    The nth argument must be positive integer starting from 1.
+
+    For example, let's say you have a mock `drinksFive` function that 
+    returns the name of the beverage that was consumed. You can 
+    write:
+*/
+
+describe('drinksFive', () => {
+    test('drinksFive returns expected nth calls', () => {
+        const beverage1 = { name: 'La Croix (Lemon)' };
+        const beverage2 = { name: 'La Croix (Orange)' };
+        const drinksFive = jest.fn((beverage) => beverage.name);
+
+        drinksFive(beverage1);
+        drinksFive(beverage2);
+
+        expect(drinksFive).toHaveNthReturnedWith(1, 'La Croix (Lemon)');
+        expect(drinksFive).toHaveNthReturnedWith(2, 'La Croix (Orange)');
     });
 });
